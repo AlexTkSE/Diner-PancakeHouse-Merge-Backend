6b6c3509e9df8695c18982e0c0738359528426ef
Oleksandra Tkachuk <126991274+AlexTkSE@users.noreply.github.com>
2024-04-07 22:39:26 -0400

    part3-gw

---
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/model/DinerMenuIterator.java
@@ -0,0 +1,40 @@
+package edu.iu.habahram.DinerPancakeHouseMerge.model;
+
+import java.util.Iterator;
+
+public class DinerMenuIterator implements Iterator {
+    MenuItem[] menuItems;
+    int position = 0;
+
+    public DinerMenuIterator(MenuItem[] menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(position >= menuItems.length || (menuItems[position] == null)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public MenuItem next() {
+        MenuItem menuItem = menuItems[position];
+        position += 1;
+        return menuItem;
+    }
+
+    public void remove() {
+        if (position <= 0) {
+            throw new IllegalStateException
+                    ("Can't remove an item");
+        }
+        if (menuItems[position-1] != null) {
+            for (int i = position-1; i < (menuItems.length-1); i++) {
+                menuItems[i] = menuItems[i+1];
+            }
+            menuItems[menuItems.length-1] = null;
+        }
+    }
+}
