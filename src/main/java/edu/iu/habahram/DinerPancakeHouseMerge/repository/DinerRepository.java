62755912a06c1a8165b1c4d38ff64d41b14649b5
Oleksandra Tkachuk <126991274+AlexTkSE@users.noreply.github.com>
2024-04-07 22:14:07 -0400

    part2-gw

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/DinerRepository.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/repository/DinerRepository.java
@@ -3,8 +3,8 @@ package edu.iu.habahram.DinerPancakeHouseMerge.repository;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.DinerMenu;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
 import org.springframework.stereotype.Repository;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.Iterator;
 
-import java.util.List;
 
 @Repository
 public class DinerRepository {
@@ -13,4 +13,11 @@ public class DinerRepository {
         DinerMenu dinerMenu = new DinerMenu();
         return dinerMenu.getMenuItems();
     }
+
+    public Iterator getTheMenuIterator(){
+        DinerMenu dinerMenu = new DinerMenu();
+        return dinerMenu.createIterator();
+    }
+
+
 }
