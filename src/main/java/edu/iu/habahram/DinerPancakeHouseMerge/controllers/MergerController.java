e08d909dac7b1cbd81ff353b5503f52f1bd71974
Oleksandra Tkachuk <126991274+AlexTkSE@users.noreply.github.com>
2024-04-07 23:47:00 -0400

    part7-gw

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,15 +1,12 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
-
-import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
-import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
-import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItemRecord;
+import edu.iu.habahram.DinerPancakeHouseMerge.repository.MergerRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,27 +14,15 @@ import java.util.List;
 @RequestMapping("/merger")
 public class MergerController {
 
-    DinerRepository dinerRepository;
-    PancakeHouseRepository pancakeHouseRepository;
+    MergerRepository mergerRepository;
 
-    public MergerController(PancakeHouseRepository pancakeHouseRepository, DinerRepository dinerRepository){
-        this.pancakeHouseRepository = pancakeHouseRepository;
-        this.dinerRepository = dinerRepository;
+    public MergerController(MergerRepository mergerRepository) {
+        this.mergerRepository = mergerRepository;
     }
 
     @GetMapping
-    public List<MenuItem> get(){
-        List<MenuItem> pancakeHouseMenu = pancakeHouseRepository.getTheMenu();
-        MenuItem[] dinerMenu = dinerRepository.getTheMenu();
-
-        List<MenuItem> splitMenu = new ArrayList<>();
-        for (MenuItem i : pancakeHouseMenu){
-            splitMenu.add(i);
-        }
-
-        for (MenuItem i : dinerMenu){
-            splitMenu.add(i);
-        }
-        return splitMenu;
+    public List<MenuItemRecord> get() {
+        List<MenuItemRecord> items = mergerRepository.getTheMenuItems();
+        return items;
     }
 }
