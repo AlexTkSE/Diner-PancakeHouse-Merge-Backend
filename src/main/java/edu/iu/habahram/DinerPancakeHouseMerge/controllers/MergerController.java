bb0e197b2615c50796cf15b641e449ddd1f626a0
Oleksandra Tkachuk <126991274+AlexTkSE@users.noreply.github.com>
2024-04-07 22:07:36 -0400

    part2-gw

--- src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
+++ src/main/java/edu/iu/habahram/DinerPancakeHouseMerge/controllers/MergerController.java
@@ -1,6 +1,7 @@
 package edu.iu.habahram.DinerPancakeHouseMerge.controllers;
 
 
+import edu.iu.habahram.DinerPancakeHouseMerge.model.DinerMenuIterator;
 import edu.iu.habahram.DinerPancakeHouseMerge.model.MenuItem;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.DinerRepository;
 import edu.iu.habahram.DinerPancakeHouseMerge.repository.PancakeHouseRepository;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import edu.iu.habahram.DinerPancakeHouseMerge.model.Iterator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,17 +29,22 @@ public class MergerController {
 
     @GetMapping
     public List<MenuItem> get(){
-        List<MenuItem> pancakeHouseMenu = pancakeHouseRepository.getTheMenu();
         MenuItem[] dinerMenu = dinerRepository.getTheMenu();
 
-        List<MenuItem> splitMenu = new ArrayList<>();
-        for (MenuItem i : pancakeHouseMenu){
-            splitMenu.add(i);
+        Iterator dinerIter = new DinerMenuIterator(dinerMenu);
+        List<MenuItem> splitDinerMenuList = new ArrayList<>();
+
+        while (dinerIter.hasNext()){
+            splitDinerMenuList.add((MenuItem) dinerIter.next());
         }
 
-        for (MenuItem i : dinerMenu){
-            splitMenu.add(i);
+        Iterator pancakeHouseIter = new DinerMenuIterator(dinerMenu);
+        List<MenuItem> splitPancakeHouseMenuList = new ArrayList<>();
+
+        while (pancakeHouseIter.hasNext()){
+            splitPancakeHouseMenuList.add((MenuItem) pancakeHouseIter.next());
         }
-        return splitMenu;
+
+        return splitDinerMenuList;
     }
 }
